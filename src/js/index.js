769592598fc7a084b431642f799a5ae73de11d62
Glitch (pingobras-the-last-of-us) <none>
2023-03-12 00:54:12 +0000

    ↘️🚇 Updated with Glitch

---
+++ src/js/index.js
@@ -0,0 +1,23 @@
+const btnCarrossel = document.querySelectorAll(".btn");
+const Img = document.querySelectorAll(".imagem")
+
+
+btnCarrossel.forEach((botao, ID) => {
+    botao.addEventListener("click", () => {
+        desativarBtn()
+        esconderImgAtiva()
+        botao.classList.add("selecionado");
+        
+        Img[ID].classList.add("ativa");
+    })
+})
+
+function desativarBtn(){
+    const btnSelecionado = document.querySelector(".selecionado");
+    btnSelecionado.classList.remove("selecionado");
+}
+
+function esconderImgAtiva(){
+    const imagemAtiva = document.querySelector(".ativa");
+    imagemAtiva.classList.remove("ativa");
+}
